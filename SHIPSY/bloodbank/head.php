230987919b2bce7f3d00a577e6c7168edf20963c
naganihita <165452592+naganihita@users.noreply.github.com>
2024-05-11 10:22:08 +0530

    Delete SHIPSY directory

--- SHIPSY/bloodbank/head.php
+++
@@ -1,9 +0,0 @@
-<head>
-	<title><?php echo $title ?></title>
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
